3a2065552a44a2b3561c842d4d0631f900af6466
deep-ydv <work.pradeepydv@gmail.com>
2025-08-03 18:23:40 +0530

    UI update

---
+++ frontend/src/components/ui/TaskWaveLogo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const TaskWaveLogo = () => {
+  return (
+    
+  <img className='w-30 sm:w-40 md:w-50' src="./public/whitelogo.png" alt="" />
+  )
+}
+
+export default TaskWaveLogo
